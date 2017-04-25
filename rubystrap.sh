ef632957a161353f5786182976df5afb45778ade
Gazaunga <Gazaunga@gmail.com>
2017-04-25 14:56:25 -0400

    Create rubystrap.sh

---
+++ rubystrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pacaur -S --noconfirm --noedit ruby rbenv
+echo "PATH='$(ruby -e 'print Gem.user_dir')/bin:$PATH'" >> ~/.bashrc
+echo "GEM_HOME=$(ls -t -U | ruby -e 'puts Gem.user_dir')" >> ~/.bashrc
+echo "GEM_PATH=$GEM_HOME" >> ~/.bashrc
+echo "export PATH=$PATH:$GEM_HOME/bin" >> ~/.bashrc
+echo "export GEM_HOME=$(ruby -e 'print Gem.user_dir')" >> ~/.bashrc
+touch ~/.gemrc
+gem install bundler
+gem install jekyll
+gem update
+reboot
