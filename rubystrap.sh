28a0f4d1b3cd67bd98899279c35176f580f29bd4
Gazaunga <Gazaunga@gmail.com>
2017-05-16 13:19:52 -0400

    Update rubystrap.sh

--- rubystrap.sh
+++ rubystrap.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
-pacaur -S --noconfirm --noedit ruby rbenv
-echo "PATH='$(ruby -e 'print Gem.user_dir')/bin:$PATH'" >> ~/.bashrc
-echo "GEM_HOME=$(ls -t -U | ruby -e 'puts Gem.user_dir')" >> ~/.bashrc
-echo "GEM_PATH=$GEM_HOME" >> ~/.bashrc
-echo "export PATH=$PATH:$GEM_HOME/bin" >> ~/.bashrc
-echo "export GEM_HOME=$(ruby -e 'print Gem.user_dir')" >> ~/.bashrc
+source ~/rubydev/bin/utils/sh
+e_success "Let's grab Ruby and ruby-docs"
+cd ~
+git clone git://github.com/sstephenson/rbenv.git .rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /etc/profile.d/rbenv.sh
+echo 'eval "$(rbenv init -)"' >> /etc/profile.d/rbenv.sh
+source /etc/profile
+rbenv install 2.4.1
+rbenv rehash
+rbenv global 2.4.1
 touch ~/.gemrc
 gem install bundler
 gem install jekyll
 gem install rake
+gem install rails
+gem install sinatra
+gem install thin
+gem install asciidoctor
+gem install ruby-doc
 gem install rpg-maker-rgss3
 gem install gepub
 gem install sass
 gem update
-exe fontstrap.sh
-./fontstrap.sh
-reboot
+git config --global user.name "Gazaunga"
+git config --global user.email "jeremy.ottley@gmail.com"
+git config --global core.autocrlf input
+git config --global core.safecrlf true
+sudo gem install --no-user-instal rails rake jekyll bundler
+bundle install --path vendor/bundle 
+sudo gem rdoc --all --ri --no-rdoc
+sudo chmod +x ~/bin/fontstrap.sh
+bash fontstrap.sh
+
